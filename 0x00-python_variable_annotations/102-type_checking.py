5a2e2ede771d9d0ed2eb0be88cdeca52d4b22a2a
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-01-11 11:35:52 +0200

    Task12[Edit]: Type Checking {function loop}

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -4,10 +4,10 @@ from typing import List, Tuple
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
-    """"""
+    """Type Checking Exercise"""
     zoomed_in: List = [
         item for item in lst
-        for _ in range(factor)
+        for i in range(int(factor))
     ]
     return zoomed_in
 
