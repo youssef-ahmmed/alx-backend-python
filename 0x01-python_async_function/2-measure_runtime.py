dbcecf46500d2b0ab240838fcd85e3fbcd89b920
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-01-21 15:40:40 +0200

    Task2[Add]: measure the runtime

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Measure the runtime"""
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """function that call wait_n and calculate the total time"""
+    start = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    total_time = time.perf_counter() - start
+
+    return total_time / n
