07e29486bb0eec2463f25273d07835403138fbe8
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-03 09:03:26 +0200

    Task0[Edit]: Implement async Generator {type annotation}

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,11 +4,12 @@
 import asyncio
 import random
 
+from typing import Generator
 
-async def async_generator():
+
+async def async_generator() -> Generator[float, None, None]:
     """ Function that loop 10 times, each time asynchronously wait 1 second,
     then yield a random number between 0 and 10"""
     for _ in range(10):
-        random_number: float = random.uniform(0, 10)
         await asyncio.sleep(1)
-        yield random_number
+        yield random.uniform(0, 10)
