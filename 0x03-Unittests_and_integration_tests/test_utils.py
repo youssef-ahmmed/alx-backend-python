58cd87aa09afcef80c5b33e32d56e1ff89937ad5
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-02-05 23:24:58 +0200

    Task2[Edit]: Test with mocking HTTP calls {docstring in test class methods}

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -57,10 +57,12 @@ class TestMemoize(unittest.TestCase):
         class TestClass:
             """Implement test class"""
             def a_method(self):
+                """a_method"""
                 return 42
 
             @memoize
             def a_property(self):
+                """a_property"""
                 return self.a_method()
 
         with patch.object(TestClass, 'a_method',
